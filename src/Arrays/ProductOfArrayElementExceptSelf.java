aec6982a4f2080e32b08a44a933e92c7283a393b
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-07-29 23:35:58 +0530

    ProductOfArrayElmentExceptSelf

---
+++ src/Arrays/ProductOfArrayElementExceptSelf.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+
+import java.util.Arrays;
+
+public class ProductOfArrayElementExceptSelf {
+    public static int[] productOfArrayElementExceptSelf(int[] nums){
+        int len=nums.length;
+        int[] ans=new int[len];
+        int prodL=1;
+        int prodR=1;
+        for (int i=len-1; i>=0; i--){
+            ans[i]=prodR;
+            prodR=prodR*nums[i];
+        }
+        for (int i=0; i<len;i++){
+            ans[i]=ans[i]*prodL;
+            prodL=prodL*nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int[] ints = productOfArrayElementExceptSelf(arr);
+        System.out.println(Arrays.toString(ints));
+    }
+}
+
