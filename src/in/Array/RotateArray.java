f7915a75a64a5f6c54c34d6a553a5b278fa0b983
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-08-11 21:41:56 +0530

    RotateArray

---
+++ src/in/Array/RotateArray.java
@@ -0,0 +1,24 @@
+package in.Array;
+
+
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void rotateArray(int[] arr){
+       int i=0;
+       int j=arr.length-1;
+       while (i!=j){
+           int temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           i++;
+       }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        rotateArray(arr);
+    }
+}
