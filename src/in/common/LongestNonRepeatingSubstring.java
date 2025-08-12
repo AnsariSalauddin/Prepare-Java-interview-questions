94b7c21198ee1111318be3df0056170a94842cd8
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-08-12 22:17:40 +0530

    LongestNonRepeatingSubstring

---
+++ src/in/common/LongestNonRepeatingSubstring.java
@@ -0,0 +1,39 @@
+package in.common;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestNonRepeatingSubstring {
+    public static void nonRepeatingSubstring(String ss){
+        int longest=0;
+        int start=0;
+        int end=0;
+        List<Character> list=new ArrayList<>();
+        //List<String> lls=new ArrayList<>();
+
+      while (end<ss.length()){
+          if(!list.contains(ss.charAt(end))){
+              list.add(ss.charAt(end));
+              longest=Math.max(longest, list.size());
+              end++;
+          }else {
+
+              list.remove(Character.valueOf(ss.charAt(start)));
+              start++;
+          }
+      }
+
+
+        System.out.println(longest);
+
+    }
+    public static void main(String[] args) {
+       String string="abcefabcbb";
+       nonRepeatingSubstring(string);
+
+
+    }
+}
+
