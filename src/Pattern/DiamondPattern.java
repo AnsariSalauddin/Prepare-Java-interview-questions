94e1b22ba76d0fded9e52bd68d365982ba756e20
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-09-06 23:06:57 +0530

    DiamondPattern

---
+++ src/Pattern/DiamondPattern.java
@@ -0,0 +1,29 @@
+package in.Pattern;
+
+public class DiamondPattern {
+    public static void diamondPatter(int n){
+        for (int i=1; i<=n;i++){
+            for (int j=1; j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=i; j++){
+                System.out.print(" *  ");
+            }
+            System.out.println();
+        }
+        for (int i=1; i<=n;i++){
+            for (int j=1 ;j<=i;j++){
+                System.out.print("  ");
+            }
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" *  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+    int n=6;
+    diamondPatter(n);
+    }
+}
+
