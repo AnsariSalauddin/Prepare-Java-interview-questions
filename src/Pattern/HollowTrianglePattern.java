a9d92ca74c4ae1fe8270f4b67a39df015489bd70
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-09-06 23:07:44 +0530

    HollowTrianglePattern

---
+++ src/Pattern/HollowTrianglePattern.java
@@ -0,0 +1,13 @@
+package in.Pattern;
+
+public class HollowTrianglePattern {
+    public static void hollowTrianglePattern(int n){
+        for (int i=1; i<=n;i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        hollowTrianglePattern(n);
+    }
+}
